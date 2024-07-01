672bfd9d7251b88c7343bff1e1ed86e31d7184a6
Tatyana <tatyanalipch@yandex.ru>
2024-07-01 17:27:53 +0300

    .gitignore

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        if (amount < 1000) {
+            return boundary - amount % boundary;
+        }
+        return amount;
+    }
+}
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void lessThanBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void moreThanBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 2000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 2000;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void boundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+}
+
