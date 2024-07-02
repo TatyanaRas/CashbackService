e75b3a42c355f790b2cee68e0ab537c927309151
Tatyana <tatyanalipch@yandex.ru>
2024-07-03 00:07:35 +0300

    src/

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
- import org.testng.Assert;
+import org.testng.Assert;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
