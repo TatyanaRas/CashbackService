543ca1af49f0289989b51832e02adaa4f7e09ceb
Tatyana <tatyanalipch@yandex.ru>
2024-07-03 10:46:41 +0300

    build.gradle

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -34,5 +34,23 @@ class CashbackHackServiceTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void boundary1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void boundary2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
 
